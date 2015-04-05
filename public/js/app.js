0eef3ceaf2378c5ef34398c9f2b6d2bea1b78cf3
erw57 <stonewe123@gmail.com>
2015-04-04 21:46:31 -0400

    add new product page, including image upload

--- public/js/app.js
+++ public/js/app.js
@@ -15,8 +15,10 @@ var customerApp = angular.module('customerApp',[
 
 var inventoryApp = angular.module('inventoryApp', [
   'ngRoute',
+  'angularFileUpload',
+  'angular-file-upload',
+  'angular-bootstrap-select',
   'inventoryController',
-  'angular-bootstrap-select'
 ]);
 
 productBrowseApp.directive('modal', function () {
@@ -165,8 +167,6 @@ inventoryApp.config(['$routeProvider',
   }
 ]);
 
-
-
 //Bootstrap select
 angular.module('angular-bootstrap-select', [])
   .directive('selectpicker', ['$parse', function ($parse) {
@@ -192,4 +192,52 @@ angular.module('angular-bootstrap-select', [])
       }
     };
   }]);
-//})();
+
+//angular-file-upload
+angular.module('angular-file-upload',[])
+  .directive('ngThumb', ['$window', function($window) {
+        var helper = {
+            support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+            isFile: function(item) {
+                return angular.isObject(item) && item instanceof $window.File;
+            },
+            isImage: function(file) {
+                var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        };
+
+        return {
+            restrict: 'A',
+            template: '<canvas/>',
+            link: function(scope, element, attributes) {
+                if (!helper.support) return;
+
+                var params = scope.$eval(attributes.ngThumb);
+
+                if (!helper.isFile(params.file)) return;
+                if (!helper.isImage(params.file)) return;
+
+                var canvas = element.find('canvas');
+                var reader = new FileReader();
+
+                reader.onload = onLoadFile;
+                reader.readAsDataURL(params.file);
+
+                function onLoadFile(event) {
+                    var img = new Image();
+                    img.onload = onLoadImage;
+                    img.src = event.target.result;
+                }
+
+                function onLoadImage() {
+                    var width = params.width || this.width / this.height * params.height;
+                    var height = params.height || this.height / this.width * params.width;
+                    canvas.attr({ width: width, height: height });
+                    canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+                }
+            }
+        };
+    }]);
+
+
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -75,6 +75,7 @@ productBrowseControllers.controller('cartController', ['$scope', '$http',  funct
 productBrowseControllers.controller('storeSelectController',['$scope','$http',
 	function($scope,$http){
 		$scope.storeList = [];
+		$scope.type ='product';
 		$http.get('api/getStoreList').success(function(res){
 			$scope.storeList = res.data;
 			$scope.store = '';
@@ -164,6 +165,7 @@ customerControllers.controller('customerNewController', ['$scope','$http',functi
 inventoryControllers.controller('storeSelectController',['$scope','$http',
 	function($scope,$http){
 		$scope.storeList = [];
+		$scope.type = 'inventory';
 		$http.get('api/getStoreList').success(function(res){
 			$scope.storeList = res.data;
 			$scope.store = '';
@@ -232,7 +234,56 @@ inventoryControllers.controller('inventoryListController',['$scope','$routeParam
 		}
 }]);
 
-inventoryControllers.controller('inventoryNewController',['$scope',function($scope){
-	console.log('inventory new');
-}])
+inventoryControllers.controller('inventoryNewController', ['$scope', 'FileUploader', function($scope, FileUploader) {
+        var uploader = $scope.uploader = new FileUploader({
+            url: 'upload.php'
+        });
 
+        // FILTERS
+
+        uploader.filters.push({
+            name: 'imageFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        });
+
+        // CALLBACKS
+
+        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        uploader.onAfterAddingFile = function(fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+        };
+        uploader.onAfterAddingAll = function(addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        uploader.onBeforeUploadItem = function(item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        uploader.onProgressItem = function(fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        uploader.onProgressAll = function(progress) {
+            console.info('onProgressAll', progress);
+        };
+        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        uploader.onErrorItem = function(fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        uploader.onCancelItem = function(fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
+    }]);
