532896c464531e5c9deff4b89a11484c6d0a02a6
erw57 <stonewe123@gmail.com>
2015-04-06 16:26:16 -0400

    add file upload module, waiting for back end

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -236,10 +236,11 @@ inventoryControllers.controller('inventoryListController',['$scope','$routeParam
 
 inventoryControllers.controller('inventoryNewController', ['$scope', 'FileUploader', function($scope, FileUploader) {
         var uploader = $scope.uploader = new FileUploader({
-            url: 'upload.php'
-        });
-
-        // FILTERS
+	            url: 'upload.php',
+	            formData: [],
+	            //formData: {newProduct:$scope.newProduct},
+	        });
+        //FILTERS
 
         uploader.filters.push({
             name: 'imageFilter',
@@ -249,41 +250,49 @@ inventoryControllers.controller('inventoryNewController', ['$scope', 'FileUpload
             }
         });
 
+        // $scope.confirm = function(){
+        // 	var uploader = $scope.uploader = new FileUploader({
+	       //      url: 'upload.php',
+	       //      formData: [],
+	       //      //formData: {newProduct:$scope.newProduct},
+	       //  });
+        // 	uploader.uploadAll();
+        // }
         // CALLBACKS
 
-        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
-            console.info('onWhenAddingFileFailed', item, filter, options);
-        };
-        uploader.onAfterAddingFile = function(fileItem) {
-            console.info('onAfterAddingFile', fileItem);
-        };
-        uploader.onAfterAddingAll = function(addedFileItems) {
-            console.info('onAfterAddingAll', addedFileItems);
-        };
+        // uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+        //     console.info('onWhenAddingFileFailed', item, filter, options);
+        // };
+        // uploader.onAfterAddingFile = function(fileItem) {
+        //     console.info('onAfterAddingFile', fileItem);
+        // };
+        // uploader.onAfterAddingAll = function(addedFileItems) {
+        //     console.info('onAfterAddingAll', addedFileItems);
+        // };
         uploader.onBeforeUploadItem = function(item) {
-            console.info('onBeforeUploadItem', item);
+        	item.formData.push({newProduct: $scope.newProduct});
         };
-        uploader.onProgressItem = function(fileItem, progress) {
-            console.info('onProgressItem', fileItem, progress);
-        };
-        uploader.onProgressAll = function(progress) {
-            console.info('onProgressAll', progress);
-        };
-        uploader.onSuccessItem = function(fileItem, response, status, headers) {
-            console.info('onSuccessItem', fileItem, response, status, headers);
-        };
-        uploader.onErrorItem = function(fileItem, response, status, headers) {
-            console.info('onErrorItem', fileItem, response, status, headers);
-        };
-        uploader.onCancelItem = function(fileItem, response, status, headers) {
-            console.info('onCancelItem', fileItem, response, status, headers);
-        };
-        uploader.onCompleteItem = function(fileItem, response, status, headers) {
-            console.info('onCompleteItem', fileItem, response, status, headers);
-        };
-        uploader.onCompleteAll = function() {
-            console.info('onCompleteAll');
-        };
+        // uploader.onProgressItem = function(fileItem, progress) {
+        //     console.info('onProgressItem', fileItem, progress);
+        // };
+        // uploader.onProgressAll = function(progress) {
+        //     console.info('onProgressAll', progress);
+        // };
+        // uploader.onSuccessItem = function(fileItem, response, status, headers) {
+        //     console.info('onSuccessItem', fileItem, response, status, headers);
+        // };
+        // uploader.onErrorItem = function(fileItem, response, status, headers) {
+        //     console.info('onErrorItem', fileItem, response, status, headers);
+        // };
+        // uploader.onCancelItem = function(fileItem, response, status, headers) {
+        //     console.info('onCancelItem', fileItem, response, status, headers);
+        // };
+        // uploader.onCompleteItem = function(fileItem, response, status, headers) {
+        //     console.info('onCompleteItem', fileItem, response, status, headers);
+        // };
+        // uploader.onCompleteAll = function() {
+        //     console.info('onCompleteAll');
+        // };
 
-        console.info('uploader', uploader);
+        // console.info('uploader', uploader);
     }]);
